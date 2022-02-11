5dd9e4406b3595666c39bbc7b05351b11f09bd60
simon@automaton.systems <simon@automaton.systems>
2022-02-11 11:48:24 +0000

    Stronger auth + body typing

--- example.js
+++ example.js
@@ -1,13 +1,30 @@
-import Server from './dist/AutomatonServer.js';
-
-let server = new Server('test', Server.Auth.NO_AUTH);
-
-Server.EXTENDED_STATUS_MODE = true;
-
+import Server, { Body, StatusMode } from './dist/AutomatonServer.js';
+import fs from 'fs';
+let server = new Server();
+server.config.statusMode = StatusMode.EXTENDED;
+server.serve('/', './');
 server
 	.api('api')
+		.post('file', Body.BLOB, async (reply, {body})=>{
+			console.log(body);
+			return await reply.json({});
+		})
+		.post('array', Body.STREAM, async (reply, {body})=>{
+			console.log("writing");
+
+			// write the file to disk
+			await new Promise(res=>{
+				let stream = fs.createWriteStream('./output.zip')
+				body.pipe(stream);
+				body.on('end',res);
+			})
+
+			console.log("written");
+			//fs.writeFile('./output.zip', body, null, ()=>{console.log("done")});
+			return await reply.json({});
+		})
 		.get('item/{id:number}', async (reply, {id})=>{
 			return await reply.json({id: id});
-		})
+		});
 
 server.start(80);
